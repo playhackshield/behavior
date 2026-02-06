9cc3ca10c19dcc4656f5cc3f1a59673eb2e769bf
playhackshield <playhackshield@gmail.com>
2026-02-06 10:10:39 +0100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// Vervang deze config met je eigen Firebase config
+const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_AUTH_DOMAIN",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_STORAGE_BUCKET",
+    messagingSenderId: "YOUR_SENDER_ID",
+    appId: "YOUR_APP_ID"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
